8b1b22b94343975c18de0208fd1dea335ee468e5
Devasy Patel <110348311+Devasy23@users.noreply.github.com>
2024-11-26 20:57:23 +0530

    Release v0.1.8: Added multiple backbones and loss functions

--- deeptuner/__init__.py
+++ deeptuner/__init__.py
@@ -2,4 +2,4 @@
 DeepTuner - A package for fine-tuning deep learning models with Siamese architecture and triplet loss
 """
 
-__version__ = '0.1.8'
+__version__ = '0.1.9'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deeptuner',
-    version='0.1.8',
+    version='0.1.9',
     author='Devasy Patel',
     author_email='patel.devasy.23@gmail.com',
     description='A package for fine-tuning deep learning models with Siamese architecture and triplet loss',
